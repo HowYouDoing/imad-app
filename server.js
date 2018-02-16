62aac6fb3e3632d74427f89b4ad0b62a2ac968ad
HowYouDoing <36479728+HowYouDoing@users.noreply.github.com>
2018-02-16 20:00:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req,res) {
-    res.sendFile(path.join(__dirname, 'article-one.html'));
+    res.sendFile(path.join(__dirname,'ui', 'article-one.html'));
 });
 
 app.get('/Article-two',function(req,res) {
-    res.send('Article two is here and will be served shortly');
+    res.sendFile(path.join(__dirname,'ui', 'article-two.html'));
 });
 
 app.get('/Article-three',function(req,res){
-res.send('Article three is here and will be served shortly');
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
